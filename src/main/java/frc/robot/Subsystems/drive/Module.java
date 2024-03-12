d0555b40d215bbc270d88ecc249b01963927ce4d
Kouyang07 <kaicheng@kouyang.dev>
2024-03-12 16:33:35 -0400

    Angle offset

--- src/main/java/frc/robot/Subsystems/drive/Module.java
+++ src/main/java/frc/robot/Subsystems/drive/Module.java
@@ -46,6 +46,13 @@ public class Module {
     // separate robot with different tuning)
     switch (Constants.currentMode) {
       case REAL:
+        /*
+        driveFeedforward = new SimpleMotorFeedforward(0.1, 0.13);
+        driveFeedback = new PIDController(0.05, 0.0, 0.0);
+        turnFeedback = new PIDController(7.0, 0.0, 0.0);
+        break;
+
+         */
       case REPLAY:
         driveFeedforward = new SimpleMotorFeedforward(0.1, 0.13);
         driveFeedback = new PIDController(0.05, 0.0, 0.0);
--- src/main/java/frc/robot/Subsystems/drive/ModuleIOSparkMax.java
+++ src/main/java/frc/robot/Subsystems/drive/ModuleIOSparkMax.java
@@ -64,25 +64,25 @@ public class ModuleIOSparkMax implements ModuleIO {
         driveSparkMax = new CANSparkFlex(51, MotorType.kBrushless);
         turnSparkMax = new CANSparkMax(52, MotorType.kBrushless);
         cancoder = new CANcoder(12, "The CANivore");
-        absoluteEncoderOffset = Rotation2d.fromDegrees(108.457); // MUST BE CALIBRATED
+        absoluteEncoderOffset = Rotation2d.fromRotations(-0.288); // MUST BE CALIBRATED
         break;
       case 1: // LF
         driveSparkMax = new CANSparkFlex(61, MotorType.kBrushless);
         turnSparkMax = new CANSparkMax(62, MotorType.kBrushless);
         cancoder = new CANcoder(10, "The CANivore");
-        absoluteEncoderOffset = Rotation2d.fromDegrees(-52.9102); // MUST BE CALIBRATED
+        absoluteEncoderOffset = Rotation2d.fromRotations(0.1455); // MUST BE CALIBRATED
         break;
       case 2: // RF
         driveSparkMax = new CANSparkFlex(49, MotorType.kBrushless);
         turnSparkMax = new CANSparkMax(50, MotorType.kBrushless);
         cancoder = new CANcoder(11, "The CANivore");
-        absoluteEncoderOffset = Rotation2d.fromDegrees(158.5547); // MUST BE CALIBRATED
+        absoluteEncoderOffset = Rotation2d.fromRotations(-0.453); // MUST BE CALIBRATED
         break;
       case 3: // RR
         driveSparkMax = new CANSparkFlex(56, MotorType.kBrushless);
         turnSparkMax = new CANSparkMax(55, MotorType.kBrushless);
         cancoder = new CANcoder(13, "The CANivore");
-        absoluteEncoderOffset = Rotation2d.fromDegrees(33.8379); // MUST BE CALIBRATED
+        absoluteEncoderOffset = Rotation2d.fromRotations(-0.112); // MUST BE CALIBRATED
         break;
       default:
         throw new RuntimeException("Invalid module index");
