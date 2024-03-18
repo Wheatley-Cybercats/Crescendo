0c424619e0339892d07b1381d8ded46db6284683
Shawn Khillan <55472239+Sparten2360@users.noreply.github.com>
2024-03-18 11:35:49 -0400

    FIxed infinite bug but still note aiming correctly something wrong with finding the angle

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crescendo";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 64;
-  public static final String GIT_SHA = "1501b1f6eed18edc1edf420edba9d56b5f44c4bb";
-  public static final String GIT_DATE = "2024-03-18 08:52:15 EDT";
+  public static final int GIT_REVISION = 65;
+  public static final String GIT_SHA = "f6cd9b416478761c6d606196858d21d878b98aea";
+  public static final String GIT_DATE = "2024-03-18 11:21:04 EDT";
   public static final String GIT_BRANCH = "AdvantageKit-Rebase";
-  public static final String BUILD_DATE = "2024-03-18 11:20:09 EDT";
-  public static final long BUILD_UNIX_TIME = 1710775209767L;
+  public static final String BUILD_DATE = "2024-03-18 11:34:17 EDT";
+  public static final long BUILD_UNIX_TIME = 1710776057565L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
