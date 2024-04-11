78e8e084f0b101e0842cc01162df911a209eacbb
Kouyang07 <kaicheng@kouyang.dev>
2024-04-11 09:07:35 -0400

    Changed directory capitalization to prevent conflicts

---
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -0,0 +1,56 @@
+// Copyright 2021-2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+package frc.robot.subsystems.indexer;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
+
+public class Indexer extends SubsystemBase {
+  private final IndexerIO io;
+  private final IndexerIOInputsAutoLogged inputs = new IndexerIOInputsAutoLogged();
+
+  /** Creates a new Indexer. */
+  public Indexer(IndexerIO io) {
+    this.io = io;
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    SmartDashboard.putBoolean("hasNote", io.hasNote());
+    Logger.processInputs("Indexer", inputs);
+  }
+
+  /** Run open loop at the specified voltage. */
+  public void runVolts(double volts) {
+    io.setVoltage(volts);
+  }
+
+  /** Run closed loop at the specified velocity. */
+  public void setSpeed(double speed) {
+    io.setSpeed(speed);
+  }
+
+  public boolean hasNote() {
+    return io.hasNote();
+  }
+
+  /** Stops the Indexer. */
+  public void stop() {
+    io.stop();
+  }
+
+  /** Returns a command to run a quasistatic test in the specified direction. */
+}
---
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -0,0 +1,64 @@
+// Copyright 2021-2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+package frc.robot.subsystems.intake;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
+
+public class Intake extends SubsystemBase {
+  private final IntakeIO io;
+  private final IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
+
+  /** Creates a new Intake. */
+  public Intake(IntakeIO io) {
+    this.io = io;
+    // Configure SysId
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Intake", inputs);
+  }
+
+  /** Run open loop at the specified voltage. */
+  public void runVolts(double volts) {
+    io.setVoltage(volts);
+  }
+
+  /** Run closed loop at the specified velocity. */
+  public void setSpeed(double speed) {
+    io.setSpeed(speed);
+  }
+
+  /** Stops the Intake. */
+  public void stop() {
+    io.stop();
+  }
+
+  /**
+   * Returns a command to run a quasistatic test in the specified direction. *
+   *
+   * <p>/** Returns a command to run a dynamic test in the specified direction.
+   */
+
+  /** Returns the current velocity in RPM. */
+  /*
+  @AutoLogOutput
+  public double getVelocityRPM() {
+    return Units.radiansPerSecondToRotationsPerMinute(inputs.velocityRadPerSec);
+  }*/
+
+  /** Returns the current velocity in radians per second. */
+}
